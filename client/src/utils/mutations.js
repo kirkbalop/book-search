fef04f1a6d97a8001124430040ddbb0984ab36c6
Kirk <kirkbalop@gmail.com>
2021-05-03 14:13:28 -0700

    fixed Save Book to match typeDef input

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -33,7 +33,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-    mutation saveBook($input: savedBook!) {
+    mutation saveBook($input: BookInput!) {
         saveBook (input: $input) {
             _id
             username
